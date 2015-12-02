086a2f8457cb6f7df688d6638864d23fc3049dda
iArraylist <aimd.sawitree@gmail.com>
2015-12-02 18:24:19 +0700

    add onedrive library

---
+++ app/Library/OneDrive/onedrive-config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Your OneDrive client ID.
+ */
+define('ONEDRIVE_CLIENT_ID', '000000004016694A');
+
+/*
+ * Your OneDrive client secret.
+ */
+define('ONEDRIVE_CLIENT_SECRET', 'U30Ozap0Su7I8aDMpOmbC4M3oehKH1eN');
+
+/*
+ * Your OneDrive callback URI.
+ */
+define('ONEDRIVE_CALLBACK_URI', 'http://localhost/gtc/authenOnedrive.php');
+?>
