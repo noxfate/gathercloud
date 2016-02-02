6c79f7f1c03a7b7ab3152299ab352c95dc13c991
iArraylist <aimd.sawitree@gmail.com>
2016-02-02 14:41:20 +0700

    context v2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,7 @@ Route::get('/register', "UserController@create");
 Route::post('/register', "UserController@store");
 
 Route::resource('/home',"HomeController");
+Route::post('/download',"HomeController@download");
 Route::get('/add',"CloudController@index");
 Route::post('/add/{service}',"CloudController@add");
 Route::get('/add/{service}',"CloudController@add");
